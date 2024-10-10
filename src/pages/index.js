5990f1d5932b4afd2ff801fe882997ebaf7658a0
Shaik Tabrez <37013844+tabrezdn1@users.noreply.github.com>
2024-10-10 03:38:21 -0400

    Add source file for all pages

---
+++ src/pages/index.js
@@ -0,0 +1,74 @@
+import OurMission from "./about/OurMission";
+import HowItWorks from "./about/HowItWorks";
+
+import UnderstandingCF from "./getting-started/UnderstandingCF";
+import EligibilityRequirements from "./getting-started/EligibilityRequirements";
+import RequiredDocuments from "./getting-started/RequiredDocuments";
+
+import WhatIsExpungement from "./expungement-forms/WhatIsExpungement";
+import WhyConsiderExpungenment from "./expungement-forms/WhyConsiderExpungement";
+import ExpungementEligibilityCriteria from "./expungement-forms/ExpungementEligibilityCriteria";
+import ExpungementProcess from "./expungement-forms/ExpungementProcess";
+import ExpungementRequiredDocument from "./expungement-forms/ExpungementRequiredDocuments";
+import PostExpungementInfo from "./expungement-forms/PostExpungementInfo";
+
+import CivilCourtCommonForms from "./civil-court-forms/CivilCourtCommonForms";
+import CivilCourtOverview from "./civil-court-forms/CivilCourtOverview";
+import CivilCourtRequiredDocuments from "./civil-court-forms/CivilCourtRequiredDocuments";
+
+import FamilyCourtCommonForms from "./family-court-forms/FamilyCourtCommonForms";
+import FamilyCourtOverview from "./family-court-forms/FamilyCourtOverview";
+import FamilyCourtRequiredDocuments from "./family-court-forms/FamilyCourtRequiredDocuments";
+
+import CriminalCourtCommonForms from "./criminal-court-forms/CriminalCourtCommonForms";
+import CriminalCourtOverview from "./criminal-court-forms/CriminalCourtOverview";
+import CriminalCourtRequiredDocuments from "./criminal-court-forms/CriminalCourtRequiredDocuments";
+
+import StepByStepGuides from "./how-to-fill-forms/StepByStepGuides";
+import FrequentlyAskedQuestions from "./how-to-fill-forms/FrequentlyAskedQuestions";
+
+import LegalAidServices from "./resources/LegalAidServices";
+import CourtLocationsAndContact from "./resources/CourtLocationsAndContact";
+import GlossaryOfLegalTerms from "./resources/GlossaryOfLegalTerms";
+
+import HelpCenter from "./user-support/HelpCenter";
+import ContactUs from "./user-support/ContactUs";
+
+import PrivacyPolicy from "./terms-and-policies/PrivacyPolicy";
+import TermsOfUse from "./terms-and-policies/TermsOfUse";
+import ExpungementForm from "./ExpungementForm";
+import Home from "./Home"
+export {
+  OurMission,
+  HowItWorks,
+  UnderstandingCF,
+  EligibilityRequirements,
+  RequiredDocuments,
+  WhatIsExpungement,
+  WhyConsiderExpungenment,
+  ExpungementEligibilityCriteria,
+  ExpungementProcess,
+  ExpungementRequiredDocument,
+  PostExpungementInfo,
+  CivilCourtCommonForms,
+  CivilCourtOverview,
+  CivilCourtRequiredDocuments,
+  FamilyCourtCommonForms,
+  FamilyCourtOverview,
+  FamilyCourtRequiredDocuments,
+  CriminalCourtCommonForms,
+  CriminalCourtOverview,
+  CriminalCourtRequiredDocuments,
+  StepByStepGuides,
+  FrequentlyAskedQuestions,
+  LegalAidServices,
+  CourtLocationsAndContact,
+  GlossaryOfLegalTerms,
+  HelpCenter,
+  ContactUs,
+  PrivacyPolicy,
+  TermsOfUse,
+  
+  ExpungementForm,
+  Home
+};
