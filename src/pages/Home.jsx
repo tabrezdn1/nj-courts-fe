183b15b2094f46da0b78a10f1d7a5c5932e0818a
Shaik Tabrez <37013844+tabrezdn1@users.noreply.github.com>
2024-10-10 07:43:53 -0400

    Update layout for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,21 @@
+import { Card, CardBody, Typography } from "@material-tailwind/react";
 
-import {
-  Card,
-  CardBody,
-  CardFooter,
-  Typography,
-  CardHeader,
-} from "@material-tailwind/react";
+import Heading from "../components/Heading";
 
 const Home = () => {
-  
+  const heading = "Home";
   return (
     <>
-      <Card className="w-full p-8 h-[calc(100vh-2rem)] overflow-y-auto">
-        <CardHeader
-          floated={false}
-          shadow={false}
-          color="gray"
-          variant="gradient"
-          className="m-0 rounded-md border-b border-white/10 p-8 min-h-16"
-        >
-          <Typography
-            color="white"
-            variant="h1"
-            className="text-5xl font-normal"
-          >
-            Home
-          </Typography>
-        </CardHeader>
-        <CardBody className="p-8 font-normal">
-          <Typography>
+      <Card className="w-full h-[calc(100vh-2rem)] overflow-y-auto">
+        <Heading heading={heading} />
+        <CardBody className="font-normal px-[64px]">
+          <Typography color="gray" className="py-1 w-1/2 text-2xl">
             Welcome to the New Jersey Courts Online Forms Portal. Simplify your
             legal processes by accessing and completing court forms online. Our
             user-friendly platform guides you through each step, ensuring that
             you provide all necessary information accurately.
           </Typography>
         </CardBody>
-        <CardFooter className="mt-12 p-0"></CardFooter>
       </Card>
     </>
   );
