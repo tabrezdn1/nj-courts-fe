9f1e42c2e53b774e245fe8042c754247f7e1a00d
Shaik Tabrez <37013844+tabrezdn1@users.noreply.github.com>
2024-10-10 06:44:49 -0400

    Update layout in point list component

--- src/components/PointsList.jsx
+++ src/components/PointsList.jsx
@@ -8,37 +8,31 @@ import {
   TimelineBody,
 } from "@material-tailwind/react";
 
-const PointsList = ({listPoints}) => {
+const PointsList = ({ listPoints }) => {
   return (
-    <div className="w-[32rem] py-5">
+    <div className="w-[40rem] py-5">
       <Timeline>
         {listPoints.map((point, pointIndex) => (
           <TimelineItem key={`${point}=${pointIndex}`}>
-            
             {/* Hide line connecter for the last point */}
             {pointIndex < listPoints.length - 1 && <TimelineConnector />}
-            
+
             <TimelineHeader className="h-3">
               <TimelineIcon />
               <Typography
-                variant="h6"
+                variant="h4"
                 color="blue-gray"
                 className="leading-none"
               >
                 {point.title}
               </Typography>
             </TimelineHeader>
-            
+
             <TimelineBody className="pb-8">
-              <Typography
-                variant="small"
-                color="gray"
-                className="font-normal text-gray-600"
-              >
+              <Typography color="gray" className="font-normal text-gray-600">
                 {point.description}
               </Typography>
             </TimelineBody>
-
           </TimelineItem>
         ))}
       </Timeline>
