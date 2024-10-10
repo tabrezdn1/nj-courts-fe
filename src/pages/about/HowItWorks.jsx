3c57668b74328b4fbd4b63d832eff569073b4d27
Shaik Tabrez <37013844+tabrezdn1@users.noreply.github.com>
2024-10-10 03:34:35 -0400

    Add pages for about section

---
+++ src/pages/about/HowItWorks.jsx
@@ -0,0 +1,103 @@
+import {
+  Card,
+  CardBody,
+  Typography,
+  Timeline,
+  TimelineItem,
+  TimelineConnector,
+  TimelineHeader,
+  TimelineIcon,
+  TimelineBody,
+} from "@material-tailwind/react";
+
+import Heading from "../../components/Heading"
+
+const HowItWorks = () => {
+  const heading = "How it Works?"
+  return (
+    <>
+      <Card className="w-full p-8 h-[calc(100vh-2rem)] overflow-y-auto">
+      <Heading heading={heading}/>
+        <CardBody className="p-8 font-normal">
+          <Typography color="gray" className="py-1 w-1/2 text-2xl">
+            Our platform offers:
+          </Typography>
+          <Typography color="gray" className="py-1"></Typography>
+          <div className="w-[32rem] py-5">
+            <Timeline>
+              <TimelineItem>
+                <TimelineConnector />
+                <TimelineHeader className="h-3">
+                  <TimelineIcon />
+                  <Typography
+                    variant="h6"
+                    color="blue-gray"
+                    className="leading-none"
+                  >
+                    Easy Access
+                  </Typography>
+                </TimelineHeader>
+                <TimelineBody className="pb-8">
+                  <Typography
+                    variant="small"
+                    color="gray"
+                    className="font-normal text-gray-600"
+                  >
+                    Find all the court forms you need in one place.
+                  </Typography>
+                </TimelineBody>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineConnector />
+                <TimelineHeader className="h-3">
+                  <TimelineIcon />
+                  <Typography
+                    variant="h6"
+                    color="blue-gray"
+                    className="leading-none"
+                  >
+                    Guided Assistance
+                  </Typography>
+                </TimelineHeader>
+                <TimelineBody className="pb-8">
+                  <Typography
+                    variant="small"
+                    color="gray"
+                    className="font-normal text-gray-600"
+                  >
+                    Step-by-step instructions to help you complete forms
+                    correctly.
+                  </Typography>
+                </TimelineBody>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineHeader className="h-3">
+                  <TimelineIcon />
+                  <Typography
+                    variant="h6"
+                    color="blue-gray"
+                    className="leading-none"
+                  >
+                    Secure Submission
+                  </Typography>
+                </TimelineHeader>
+                <TimelineBody>
+                  <Typography
+                    variant="small"
+                    color="gary"
+                    className="font-normal text-gray-600"
+                  >
+                    Submit your forms online securely or print them for
+                    in-person filing.
+                  </Typography>
+                </TimelineBody>
+              </TimelineItem>
+            </Timeline>
+          </div>
+        </CardBody>
+      </Card>
+    </>
+  );
+};
+
+export default HowItWorks;
