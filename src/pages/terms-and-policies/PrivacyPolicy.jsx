4b68b9010510758b1f42a067804387d61200b03d
Shaik Tabrez <37013844+tabrezdn1@users.noreply.github.com>
2024-10-10 03:37:37 -0400

    Add pages for terms and policies section

---
+++ src/pages/terms-and-policies/PrivacyPolicy.jsx
@@ -0,0 +1,27 @@
+import {
+    Card,
+    CardBody,
+    Typography,
+  } from "@material-tailwind/react";
+  import Heading from "../../components/Heading"
+const PrivacyPolicy = () => {
+    const heading = "Privacy Policy";
+    return (
+      <>
+        <Card className="w-full p-8 h-[calc(100vh-2rem)] overflow-y-auto">
+          <Heading heading={heading}/>
+          <CardBody className="p-8 font-normal">
+            <Typography color="gray" className="py-1 w-1/2 text-2xl">
+              We are dedicated to making the legal system more accessible to the
+              residents of New Jersey. Our goal is to simplify the process of
+              filling out court forms by providing clear instructions and online
+              tools, reducing the stress and confusion often associated with legal
+              paperwork.
+            </Typography>
+          </CardBody>
+        </Card>
+      </>
+    );
+}
+
+export default PrivacyPolicy;
