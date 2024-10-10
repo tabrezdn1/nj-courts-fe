23f0a4e889405278c8a32ca32e17e728d3304178
Shaik Tabrez <37013844+tabrezdn1@users.noreply.github.com>
2024-10-10 07:44:29 -0400

    Add terms and policies pages

--- src/pages/terms-and-policies/PrivacyPolicy.jsx
+++ src/pages/terms-and-policies/PrivacyPolicy.jsx
@@ -1,21 +1,33 @@
 import { Card, CardBody, Typography } from "@material-tailwind/react";
 
 import Heading from "../../components/Heading";
+import PointsList from "../../components/PointsList";
 
 const PrivacyPolicy = () => {
   const heading = "Privacy Policy";
+  const listPoints = [
+    {
+      title: "Data Protection",
+      description: " Measures we take to secure your personal information.",
+    },
+    {
+      title: "Data Usage",
+      description: "How your information is used within our services.",
+    },
+    {
+      title: "Cookies and Tracking",
+      description: "Information on website tracking technologies.",
+    },
+  ];
   return (
     <>
       <Card className="w-full h-[calc(100vh-2rem)] overflow-y-auto">
         <Heading heading={heading} />
         <CardBody className="font-normal px-[64px]">
           <Typography color="gray" className="py-1 w-1/2 text-2xl">
-            We are dedicated to making the legal system more accessible to the
-            residents of New Jersey. Our goal is to simplify the process of
-            filling out court forms by providing clear instructions and online
-            tools, reducing the stress and confusion often associated with legal
-            paperwork.
+            Policy:
           </Typography>
+          <PointsList listPoints={listPoints} />
         </CardBody>
       </Card>
     </>
--- src/pages/terms-and-policies/TermsOfUse.jsx
+++ src/pages/terms-and-policies/TermsOfUse.jsx
@@ -1,21 +1,34 @@
 import { Card, CardBody, Typography } from "@material-tailwind/react";
 
 import Heading from "../../components/Heading";
+import PointsList from "../../components/PointsList";
 
 const TermsOfUse = () => {
   const heading = "Terms of use";
+  const listPoints = [
+    {
+      title: "Acceptable Use",
+      description: "Rules governing how you may use the portal.",
+    },
+    {
+      title: "Disclaimer",
+      description:
+        "Limitation of liability regarding the information provided.",
+    },
+    {
+      title: "Modification of Terms",
+      description: "How and when terms may be updated.",
+    },
+  ];
   return (
     <>
       <Card className="w-full h-[calc(100vh-2rem)] overflow-y-auto">
         <Heading heading={heading} />
         <CardBody className="font-normal px-[64px]">
           <Typography color="gray" className="py-1 w-1/2 text-2xl">
-            We are dedicated to making the legal system more accessible to the
-            residents of New Jersey. Our goal is to simplify the process of
-            filling out court forms by providing clear instructions and online
-            tools, reducing the stress and confusion often associated with legal
-            paperwork.
+            Terms and Conditions applies*
           </Typography>
+          <PointsList listPoints={listPoints} />
         </CardBody>
       </Card>
     </>
