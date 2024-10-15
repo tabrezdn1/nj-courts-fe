1a01696e5c45f240b0f959145e2c6d818eaeadcb
Shaik Tabrez <37013844+tabrezdn1@users.noreply.github.com>
2024-10-15 17:12:11 -0400

    Fix header styles

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -7,7 +7,7 @@ const Heading = ({ heading }) => {
       shadow={false}
       color="teal"
       variant="gradient"
-      className="rounded-md border-b border-white/10 p-4 min-h-10"
+      className="rounded-md border-b border-white/10 p-4 min-h-56"
     >
       <Typography color="white" className="text-7xl mt-[120px] px-[35px]">
         {heading}
