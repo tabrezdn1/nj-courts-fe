1ee95a2ceebab9acfe6abb372c7040b55886c8f7
Runjeeth Nikam <nrunjeeth@gmail.com>
2024-10-21 13:37:20 -0400

    handled nex/prev button

--- src/components/FormStepper.jsx
+++ src/components/FormStepper.jsx
@@ -3,16 +3,30 @@ import { Stepper, Step, Button } from "@material-tailwind/react";
 import { CogIcon, UserIcon, HomeIcon } from "@heroicons/react/24/outline";
 import FormRenderer from "./FormRenderer";
 
-const GPTStepper = ({ steps }) => {
+const GPTStepper = ({ 
+  steps, 
+  moveNextTab,
+  movePrevTab,
+  isFirstTab,
+  isLastTab, 
+}) => {
   const [activeStep, setActiveStep] = React.useState(0);
   const [isLastStep, setIsLastStep] = React.useState(false);
   const [isFirstStep, setIsFirstStep] = React.useState(false);
 
   const handleNext = () => {
-    if (!isLastStep) setActiveStep((cur) => cur + 1);
+    if (!isLastStep) {
+      setActiveStep((cur) => cur + 1);
+    } else if (isLastStep && !isLastTab) {
+      moveNextTab();
+    }
   };
   const handlePrev = () => {
-    if (!isFirstStep) setActiveStep((cur) => cur - 1);
+    if (!isFirstStep) {
+      setActiveStep((cur) => cur - 1);
+    } else if (isFirstStep && !isFirstTab) {
+      movePrevTab();
+    }
   };
 
   React.useEffect(() => {
@@ -43,10 +57,10 @@ const GPTStepper = ({ steps }) => {
         <FormRenderer form={steps[activeStep]} />
       </div>
       <div className="flex justify-between mb-5">
-        <Button onClick={handlePrev} disabled={isFirstStep}>
+        <Button onClick={handlePrev} disabled={isFirstTab && isFirstStep}>
           Prev
         </Button>
-        <Button onClick={handleNext} disabled={isLastStep}>
+        <Button onClick={handleNext} disabled={isLastTab && isLastStep}>
           Next
         </Button>
       </div>
