3ec774f1ba6a75da96dc0c7ee60ccf036792a1d7
Hannan Desai <hannan.desai@inventyv-git.com>
2024-10-22 09:13:40 -0400

    home screen questionnaire commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,86 @@
-import { Card, CardBody, Typography } from "@material-tailwind/react";
+import { Card, CardBody, Typography, CardFooter, Button, Input } from "@material-tailwind/react";
+import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
 
 import Heading from "../components/Heading";
-import NavigateLink from "../components/NavigateLink";
-
+import questions from "../data/homescreen.json";
 const Home = () => {
-  const heading = "Home";
+  const [currentQuestion, setCurrentQuestion] = useState(questions[0]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [userSelection, setUserSelection] = useState({});
+  const navigate = useNavigate();
+  const heading = "NJ Courts E-Forms Portal";
+
+  const routeTo = (card) => {
+    // selection for first question
+    if (card.formLink) {
+      userSelection.formLink = card.formLink;
+      userSelection.helpLink = card.helpLink;
+    }
+
+    // selection for second question
+    if (card.actionType) {
+      userSelection.actionType = card.actionType;
+    }
+
+    // selection for third question
+    if (card.userType) {
+      userSelection.userType = card.userType;
+    }
+
+    setUserSelection(userSelection);
+
+    if (card.noQuestionForward) {
+      if (userSelection.actionType === "checkEligibility") {
+        navigate(userSelection.helpLink);
+      } else if (userSelection.actionType === "newForm" || userSelection.actionType === "continueForm") {
+        navigate(userSelection.formLink);
+      }
+    } else {
+      setCurrentQuestion(questions[currentQuestionIndex + 1]);
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }
+  }
+
+  const back = () => {
+    setCurrentQuestion(questions[currentQuestionIndex - 1]);
+    setCurrentQuestionIndex(currentQuestionIndex - 1);
+  }
+
   return (
     <>
-      <Card className="w-full h-[calc(100vh-2rem)] overflow-y-auto">
+      <div className="w-full h-[calc(100vh-2rem)] overflow-y-auto">
         <Heading heading={heading} />
-        <CardBody className="font-normal px-[64px]">
-          <Typography color="gray" className="py-1 w-1/2 text-2xl">
-            Welcome to the New Jersey Courts Online Forms Portal. Simplify your
-            legal processes by accessing and completing court forms online. Our
-            user-friendly platform guides you through each step, ensuring that
-            you provide all necessary information accurately.
-          </Typography>
-          <Typography
-            color="blue-gray"
-            className="py-1 w-1/2 text-2xl "
-            variant="h4"
-          >
-            <NavigateLink
-              label="Take me to expungement form"
-              link="./expungement-forms/form"
-            />
-          </Typography>
-        </CardBody>
-      </Card>
+        <Typography color="gray" className="pt-[55px] pb-4 px-4 text-2xl text-center font-bold">
+          {currentQuestion.question}
+        </Typography>
+        <div className={`grid grid-cols-${currentQuestion.noOfCols} gap-[30px] px-[${currentQuestion.noOfCols == 3 ? "200px" : "350px"}] pt-4`}>
+          {
+            currentQuestion.options.map((card, index) => (
+              <Card key={index} className="w-120 h-60 cursor-pointer" onClick={() => routeTo(card)}>
+                <CardBody className="flex justify-center items-center text-center">
+                  {card.svg && (
+                    <div dangerouslySetInnerHTML={{ __html: card.svg }} />
+                  )}
+                </CardBody>
+                <CardFooter className="pt-0 text-center">
+                  <Typography variant="h5" color="gray" className="mb-2">
+                    {card.title}
+                  </Typography>
+                </CardFooter>
+              </Card>
+            ))
+          }
+        </div>
+        {currentQuestion.id !== 1 && (
+          <div className="flex justify-center mt-4">
+            <Button color="teal" size="lg" className="w-auto" variant="outlined" onClick={back}>
+              Back
+            </Button>
+          </div>
+        )}
+
+      </div>
     </>
   );
 };
